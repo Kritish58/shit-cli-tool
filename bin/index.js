cba77dad85faa2d50e87e038e61ad14983ef9c47
kritish bhattarai <rishi58rishi@gmail.com>
2021-02-26 20:27:26 +0545

    added version functionality

--- bin/index.js
+++ bin/index.js
@@ -2,13 +2,20 @@
 
 const createModule = require('../lib/createModule');
 const removeModule = require('../lib/removeModule');
+const version = require('../package.json').version;
 
 // console.log(process.argv);
 const command = process.argv[2];
+const versionIsAsked = process.argv[2] === '-v' || process.argv === '--version';
+
 const module_name = process.argv[3];
 const componentExists = process.argv[4] === '-c';
 
 // with and without component
+
+if (versionIsAsked) {
+   console.log(version);
+}
 
 if (command === 'create') {
    if (componentExists) {
