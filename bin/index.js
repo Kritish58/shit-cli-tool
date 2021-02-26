806c4e6af7e33afef7a5b987ab7a5f4ec16eb8a8
kritish bhattarai <rishi58rishi@gmail.com>
2021-02-26 17:33:17 +0545

    added remove module

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 const createModule = require('../lib/createModule');
+const removeModule = require('../lib/removeModule');
 
 // console.log(process.argv);
 const command = process.argv[2];
 const module_name = process.argv[3];
 
-if (command === 'create-module') {
+if (command === 'create') {
    createModule(module_name);
+}
+
+if (command === 'remove') {
+   removeModule(module_name);
 }
