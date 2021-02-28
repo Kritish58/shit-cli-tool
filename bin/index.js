1a01d31f2187d2677bfed6dbf6c168b4233cc7a4
kritish bhattarai <rishi58rishi@gmail.com>
2021-02-28 20:45:40 +0545

    udpate

--- bin/index.js
+++ bin/index.js
@@ -10,14 +10,27 @@ const chalk = require('chalk');
 const createService = require('../lib/createService');
 
 // console.log(process.argv);
-const command = process.argv[2];
+// const command = process.argv[2];
 const versionAsked = process.argv[2] === '-v' || process.argv[2] === '--version';
 const helpAsked = process.argv[2] === '-h' || process.argv[2] === '--help';
 const createNewProject = process.argv[2] === 'new';
-const createNewService = process.argv[2] === 'service' || false;
 
-const module_name = process.argv[3];
-const componentExists = process.argv[4] === '-c';
+const createNewService =
+   (process.argv[2] === 'create' || process.argv[2] === 'c') &&
+   (process.argv[3] === 'service' || process.argv[3] === 's');
+
+const createNewModule =
+   (process.argv[2] === 'create' || process.argv[2] === 'c') &&
+   (process.argv[3] === 'module' || process.argv[3] === 'm');
+
+const removeService =
+   (process.argv[2] === 'remove' || process.argv[2] === 'rm') &&
+   (process.argv[3] === 'service' || process.argv[3] === 's');
+
+// const createNewService = process.argv[3] === 'service' || process.argv[2] === 's';
+
+// const module_name = process.argv[3];
+// const componentExists = process.argv[4] === '-c';
 
 // inside or outside component
 
@@ -38,9 +51,9 @@ if (createNewProject) {
 }
 
 if (createNewService) {
-   const serviceFileName = process.argv[3];
-   const moduleName = process.argv[4];
-   const insideComponent = process.argv[5] === '-c';
+   const serviceFileName = process.argv[4];
+   const moduleName = process.argv[5];
+   const insideComponent = process.argv[6] === '-c';
    if (!serviceFileName) {
       console.log(chalk.bold.red('please specify service name'));
       return;
@@ -60,9 +73,16 @@ if (createNewService) {
    return;
 }
 
-// for creating and removing modules
-if (command === 'create') {
-   if (componentExists) {
+if (createNewModule) {
+   const module_name = process.argv[4];
+   const insideComponent = process.argv[5] === '-c';
+   if (!module_name) {
+      console.log(chalk.bold.red('invalid module name'));
+      console.log();
+      console.log(chalk.bold.yellow('shit create module <module_name>'));
+      return;
+   }
+   if (insideComponent) {
       createModule('\\components\\' + module_name);
    } else {
       createModule('\\' + module_name);
@@ -70,12 +90,21 @@ if (command === 'create') {
    return;
 }
 
-if (command === 'remove') {
-   if (componentExists) {
-      removeModule('\\components\\' + module_name);
-   } else {
-      removeModule('\\' + module_name);
+if (process.argv[2] === 'remove' || process.argv[2] === 'rm') {
+   const module_name = process.argv[3];
+   const insideComponent = process.argv[4];
+   if (!module_name) {
+      console.log(chalk.bold.red('module name not found'));
+      console.log();
+      console.log(chalk.bold.yellow('shit remove <module_name>'));
+      return;
    }
+
+   if (insideComponent) {
+      removeModule('\\components\\' + module_name);
+      return;
+   }
+   removeModule('\\' + module_name);
    return;
 }
 
