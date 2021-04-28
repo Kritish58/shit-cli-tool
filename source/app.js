f5c215c54f2734fdd22366e9848ac0e08ece4e97
kritish bhattarai <rishi58rishi@gmail.com>
2021-04-29 01:03:45 +0545

    added helmet

--- source/app.js
+++ source/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
+app.disable('x-powered-by');
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
